838167b3df5d812b3ccba7e79f33b2216bb5e2ce
will mcfadden <wmcfadd2@gmail.com>
2016-05-26 09:10:45 -0500

    brand new figure structure

--- active/figures/fig1/load_timestamps.m
+++
@@ -1,7 +0,0 @@
-stamps = []
-for i =1:200
-    d=imfinfo(['/Volumes/will/stress/try3/fov1_' sprintf('%03d',i)  '.tif']);
-    token = strsplit(d.DateTime, ':');
-    stamps = [stamps;str2num(token{end})]; 
-end
-plot(stamps)
